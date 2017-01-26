986ac4b6b149e3244b15951ff868bc31bd8c4e39
Savas Ziplies <savas.ziplies@project-engineers.de>
2017-01-26 16:44:08 +0100

    Added: GetScrollInfo() method for convenient usage

--- MarkdownViewerPlusPlus/PluginInfrastructure/Win32.cs
+++ MarkdownViewerPlusPlus/PluginInfrastructure/Win32.cs
@@ -9,6 +9,76 @@ namespace Kbg.NppPluginNET.PluginInfrastructure
     public class Win32
     {
         /// <summary>
+        /// Get the scroll information of a scroll bar or window with scroll bar
+        /// @see https://msdn.microsoft.com/en-us/library/windows/desktop/bb787537(v=vs.85).aspx
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential)]
+        public struct ScrollInfo
+        {
+            /// <summary>
+            /// Specifies the size, in bytes, of this structure. The caller must set this to sizeof(SCROLLINFO).
+            /// </summary>
+            public uint cbSize;
+            /// <summary>
+            /// Specifies the scroll bar parameters to set or retrieve.
+            /// @see ScrollInfoMask
+            /// </summary>
+            public uint fMask;
+            /// <summary>
+            /// Specifies the minimum scrolling position.
+            /// </summary>
+            public int nMin;
+            /// <summary>
+            /// Specifies the maximum scrolling position.
+            /// </summary>
+            public int nMax;
+            /// <summary>
+            /// Specifies the page size, in device units. A scroll bar uses this value to determine the appropriate size of the proportional scroll box.
+            /// </summary>
+            public uint nPage;
+            /// <summary>
+            /// Specifies the position of the scroll box.
+            /// </summary>
+            public int nPos;
+            /// <summary>
+            /// Specifies the immediate position of a scroll box that the user is dragging. 
+            /// An application can retrieve this value while processing the SB_THUMBTRACK request code. 
+            /// An application cannot set the immediate scroll position; the SetScrollInfo function ignores this member.
+            /// </summary>
+            public int nTrackPos;
+        }
+
+        /// <summary>
+        /// Used for the ScrollInfo fMask
+        /// SIF_ALL             => Combination of SIF_PAGE, SIF_POS, SIF_RANGE, and SIF_TRACKPOS.
+        /// SIF_DISABLENOSCROLL => This value is used only when setting a scroll bar's parameters. If the scroll bar's new parameters make the scroll bar unnecessary, disable the scroll bar instead of removing it.
+        /// SIF_PAGE            => The nPage member contains the page size for a proportional scroll bar.
+        /// SIF_POS             => The nPos member contains the scroll box position, which is not updated while the user drags the scroll box.
+        /// SIF_RANGE           => The nMin and nMax members contain the minimum and maximum values for the scrolling range.
+        /// SIF_TRACKPOS        => The nTrackPos member contains the current position of the scroll box while the user is dragging it.
+        /// </summary>
+        public enum ScrollInfoMask
+        {
+            SIF_RANGE = 0x1,
+            SIF_PAGE = 0x2,
+            SIF_POS = 0x4,
+            SIF_DISABLENOSCROLL = 0x8,
+            SIF_TRACKPOS = 0x10,
+            SIF_ALL = SIF_RANGE + SIF_PAGE + SIF_POS + SIF_TRACKPOS
+        }
+
+        /// <summary>
+        /// Used for the GetScrollInfo() nBar parameter
+        /// </summary>
+        public enum ScrollInfoBar
+        {
+            SB_HORZ = 0,
+            SB_VERT = 1,
+            SB_CTL = 2,
+            SB_BOTH = 3
+        }
+
+        /// <summary>
         /// You should try to avoid calling this method in your plugin code. Rather use one of the gateways such as 
         /// <see cref="ScintillaGateway"/> or <see cref="NotepadPPGateway"/>.  
         /// If gateways are missing or incomplete, please help extend them and send your code to the project 
@@ -185,5 +255,15 @@ namespace Kbg.NppPluginNET.PluginInfrastructure
 
         [DllImport("kernel32")]
         public static extern void OutputDebugString(string lpOutputString);
+
+        /// <summary>
+        /// @see https://msdn.microsoft.com/en-us/library/windows/desktop/bb787583(v=vs.85).aspx
+        /// </summary>
+        /// <param name="hwnd"></param>
+        /// <param name="nBar"></param>
+        /// <param name="scrollInfo"></param>
+        /// <returns></returns>
+        [DllImport("user32")]
+        public static extern int GetScrollInfo(IntPtr hwnd, int nBar, ref ScrollInfo scrollInfo);
     }
 }
